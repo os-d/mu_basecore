2636e36b2b54d840d72b4f04cfdd1004961d122a
Oliver Smith-Denny <osde@microsoft.com>
2025-04-28 09:55:03 -0700

    more

--- MdeModulePkg/Core/Dxe/Gcd/Gcd.c
+++ MdeModulePkg/Core/Dxe/Gcd/Gcd.c
@@ -886,6 +886,8 @@ CoreConvertSpace (
           DEBUG ((DEBUG_ERROR, "Entry->Capabilities = %016lx\n", Entry->Capabilities));
           DEBUG ((DEBUG_ERROR, "Entry->EndAddress = %016lx\n", Entry->EndAddress));
 
+          CoreDumpGcdMemorySpaceMap (FALSE);
+
           goto Done;
         }
 
--- MdeModulePkg/Core/Dxe/Misc/MemoryProtection.c
+++ MdeModulePkg/Core/Dxe/Misc/MemoryProtection.c
@@ -264,6 +264,8 @@ SetUefiImageMemoryAttributes (
 
   CurrentAddress = BaseAddress;
 
+  DEBUG ((DEBUG_ERROR, "First entry SetUefiImageMemoryAttributes - %llx - %llx (0x%016lx)\n", BaseAddress, Length, Attributes));
+
   // we loop here because we may have multiple memory space descriptors that overlap the requested range
   // this will definitely be the case for unprotecting an image, because that calls this function for the entire image,
   // which we split into different GCD descriptors when we protected it.
@@ -281,6 +283,8 @@ SetUefiImageMemoryAttributes (
       return;
     }
 
+    DEBUG ((DEBUG_ERROR, "Got descriptor for %llx with capabilities %llx attributes trying to set: %llx, Descriptor->BaseAddress %llx Length %llx \n", CurrentAddress, Descriptor.Capabilities, Attributes, Descriptor.BaseAddress, Descriptor.Length));
+
     // ensure that we only change the attributes for the range that we are interested in, not the entire descriptor
     if (BaseAddress + Length > CurrentAddress + Descriptor.Length) {
       CurrentLength = Descriptor.Length;
@@ -291,7 +295,7 @@ SetUefiImageMemoryAttributes (
     // Preserve the existing caching and virtual attributes, but remove the hardware access bits
     FinalAttributes = (Descriptor.Attributes & ~EFI_MEMORY_ACCESS_MASK) | (Attributes & EFI_MEMORY_ATTRIBUTE_MASK);
 
-    DEBUG ((DEBUG_VERBOSE, "SetUefiImageMemoryAttributes - 0x%016lx - 0x%016lx (0x%016lx)\n", CurrentAddress, CurrentLength, FinalAttributes));
+    DEBUG ((DEBUG_ERROR, "SetUefiImageMemoryAttributes - 0x%016lx - 0x%016lx (0x%016lx)\n", CurrentAddress, CurrentLength, FinalAttributes));
 
     // check to see if the capabilities support the attributes we want to set. If not, set the capabilities appropriately
     if ((Descriptor.Capabilities & FinalAttributes) != FinalAttributes) {
@@ -385,6 +389,7 @@ SetUefiImageMemoryAttributes (
     // part of a descriptor, so we can use Descriptor.Length here to move to the next descriptor, which for the final
     // descriptor will exit the loop, regardless of whether we truncated or not
     CurrentAddress += Descriptor.Length;
+    DEBUG ((DEBUG_ERROR, "Next CurrentAddress = %llx\n", CurrentAddress));
   }
 }
 
